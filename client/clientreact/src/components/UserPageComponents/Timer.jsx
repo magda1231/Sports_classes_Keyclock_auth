9c19500a45e894ec35ee923f9a2782ffc5b0bb8c
Magda <111302737+magda1231@users.noreply.github.com>
2023-02-01 20:11:56 +0100

    clock and photo

--- client/clientreact/src/components/UserPageComponents/Timer.jsx
+++ client/clientreact/src/components/UserPageComponents/Timer.jsx
@@ -4,21 +4,32 @@ export default function Timer() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
+    client.subscribe("/time", { qos: 0 });
     client.on("message", (topic, message) => {
-      if (topic === "timer") {
-        setTime(message.toString());
+      if (topic === "/time") {
+        //get 2 last digits
+        const b = message.toString().slice(-2);
+        //get rest
+        const a = message.toString().slice(0, -2);
+
+        setTime([a, b]);
       }
     });
 
-    return () => {
-      client.off("message");
-    };
+    return () => {};
   }, []);
 
   return (
-    <div>
-      <h1>Timer</h1>
-      //<h2>{time}</h2>
+    <div
+      className="  h-min w-90 mx-auto flex flex-col  w-max  text-pink-600 my-10"
+      id="timer"
+    >
+      <h2>Nie czekaj zapisz sie juz dziś</h2>
+      <h2 className="   text-center text-pink-600">Cenny czas mija</h2>
+      <h2 className="text-center">
+        <span>{time[0]}</span>
+        <span className=" text-pink-600">{time[1]}</span>
+      </h2>
     </div>
   );
 }
